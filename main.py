5ccadb52a5a185ffb3649d1607e33b1c62887f1e
Seth Mwebi <sethmwebi27@gmail.com>
2024-12-08 09:08:21 +0300

    [Feature] Incorporate a SQL database and implement CRUD operations

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
+from database.connection import conn
 from routes.events import event_router
 from routes.users import user_router
 
-app = FastAPI()
+
+# Define the lifespan function
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup logic
+    conn()
+    yield  # Application runs here
+    # Shutdown logic (if needed)
+    print("App shutting down")
+
+
+# Initialize FastAPI with lifespan
+app = FastAPI(lifespan=lifespan)
 
 # Register routes
-
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
+
+
+@app.get("/")
+async def home():
+    return RedirectResponse(url="/event/")
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
